27926bc7bf0208a70aca7ee5c1ae441ba9a84cc7
Christopher Bacher <mindsbackyard@gmail.com>
2016-10-06 09:33:34 +0200

    Fix: restored syntex compatibility
    Feature gated the parts where syntex currently diverges from libsyntax.
    This is at best a temporary fix and probably has to be reverted once syntex is updated

--- tests/test_ty_param.rs
+++ tests/test_ty_param.rs
@@ -7,6 +7,7 @@ use aster::path::IntoPath;
 use aster::lifetime::{IntoLifetime, IntoLifetimeDef};
 
 #[test]
+#[cfg(not(feature = "with-syntex"))]
 fn test_ty_param_empty() {
     let builder = AstBuilder::new();
     let ty_param = builder.ty_param("T").build();
@@ -25,6 +26,25 @@ fn test_ty_param_empty() {
 }
 
 #[test]
+#[cfg(feature = "with-syntex")]
+fn test_ty_param_empty() {
+    let builder = AstBuilder::new();
+    let ty_param = builder.ty_param("T").build();
+
+    assert_eq!(
+        ty_param,
+        ast::TyParam {
+            ident: builder.id("T"),
+            id: ast::DUMMY_NODE_ID,
+            bounds: P::new(),
+            default: None,
+            span: DUMMY_SP,
+        }
+    );
+}
+
+#[test]
+#[cfg(not(feature = "with-syntex"))]
 fn test_ty_param_default() {
     let builder = AstBuilder::new();
     let ty_param = builder.ty_param("T")
@@ -46,6 +66,28 @@ fn test_ty_param_default() {
 }
 
 #[test]
+#[cfg(feature = "with-syntex")]
+fn test_ty_param_default() {
+    let builder = AstBuilder::new();
+    let ty_param = builder.ty_param("T")
+        .default()
+        .usize()
+        .build();
+
+    assert_eq!(
+        ty_param,
+        ast::TyParam {
+            ident: builder.id("T"),
+            id: ast::DUMMY_NODE_ID,
+            bounds: P::new(),
+            default: Some(builder.ty().usize()),
+            span: DUMMY_SP,
+        }
+    );
+}
+
+#[test]
+#[cfg(not(feature = "with-syntex"))]
 fn test_ty_param_bounds() {
     let builder = AstBuilder::new();
     let ty_param = builder.ty_param("T")
@@ -60,6 +102,58 @@ fn test_ty_param_bounds() {
         ty_param,
         ast::TyParam {
             attrs: ast::ThinVec::new(),
+            ident: builder.id("T"),
+            id: ast::DUMMY_NODE_ID,
+            bounds: P::from_vec(vec![
+                ast::TyParamBound::TraitTyParamBound(
+                    ast::PolyTraitRef {
+                        bound_lifetimes: vec![
+                            "'a".into_lifetime_def(),
+                        ],
+                        trait_ref: ast::TraitRef {
+                            path: "Trait".into_path(),
+                            ref_id: ast::DUMMY_NODE_ID,
+                        },
+                        span: DUMMY_SP,
+                    },
+                    ast::TraitBoundModifier::None,
+                ),
+                ast::TyParamBound::TraitTyParamBound(
+                    ast::PolyTraitRef {
+                        bound_lifetimes: Vec::new(),
+                        trait_ref: ast::TraitRef {
+                            path: "Sized".into_path(),
+                            ref_id: ast::DUMMY_NODE_ID,
+                        },
+                        span: DUMMY_SP,
+                    },
+                    ast::TraitBoundModifier::Maybe,
+                ),
+                ast::TyParamBound::RegionTyParamBound(
+                    "'b".into_lifetime()
+                ),
+            ]),
+            default: None,
+            span: DUMMY_SP,
+        }
+    );
+}
+
+#[test]
+#[cfg(feature = "with-syntex")]
+fn test_ty_param_bounds() {
+    let builder = AstBuilder::new();
+    let ty_param = builder.ty_param("T")
+        .bound().trait_("Trait")
+            .lifetime("'a").build()
+            .build()
+        .bound().maybe_trait("Sized").build()
+        .bound().lifetime("'b")
+        .build();
+
+    assert_eq!(
+        ty_param,
+        ast::TyParam {
             ident: builder.id("T"),
             id: ast::DUMMY_NODE_ID,
             bounds: P::from_vec(vec![
